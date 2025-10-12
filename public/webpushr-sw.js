16bf70eb491beb82976410c5b23c58186e50f4c0
Miguel-C22 <miguel.ganoza@treez.io>
2025-10-12 14:56:54 -0500

    update sw file

--- public/webpushr-sw.js
+++ public/webpushr-sw.js
@@ -1,3 +1,1 @@
-// Webpushr Service Worker
-// This file must exist at /webpushr-sw.js for Webpushr to function
-// The Webpushr SDK will inject the necessary code at runtime
+importScripts('https://cdn.webpushr.com/sw-server.min.js');
